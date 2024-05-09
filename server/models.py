d5e885dec44c9b1ac25ea0ed861554d30fa02a9e
abdishukri <hillow15521@gmail.com>
2024-05-09 19:31:56 +0300

    completed invitation and feedback

--- server/models.py
+++ server/models.py
@@ -4,10 +4,12 @@ class Invitation(db.Model):
     __tablename__ = 'invitations'
 
     id = db.Column(db.Integer, primary_key=True)
-    assessment_id = db.Column(db.Integer, db.ForeignKey('assessment.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     status = db.Column(db.String(50))
     time = db.Column(db.DateTime, default=db.func.now())
+    assessment_id = db.Column(db.Integer, db.ForeignKey('assessment.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    user = db.relationship("User", back_populates="invitations")
 
     def __repr__(self):
         return f"<Invitation(id={self.id}, status={self.status}, time={self.time})>"
@@ -16,8 +18,12 @@ class Feedback(db.Model):
     __tablename__ = 'feedbacks'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     feedback_text = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    # Establishing the one-to-many relationship with Feedback model
+    user = db.relationship("User", back_populates="feedbacks")
 
     def __repr__(self):
         return f"<Feedback(id={self.id}, feedback_text={self.feedback_text})>"
+    
